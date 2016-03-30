75480ef34c6adb32b483a50eab3e9e61a414cb3b
trahloff <tobias.rahloff@de.ibm.com>
2016-03-30 13:17:33 +0200

    typo

--- client/app/components/component1/component1.ctrl.general.js
+++ client/app/components/component1/component1.ctrl.general.js
@@ -2,31 +2,5 @@ var app = angular.module('component1Ctrls', ['ngMaterial', 'ngMessages']);
 
 app.controller('accountCtrl', ['$scope', function($scope) {
       "ngInject";
-  $scope.user = {
-    title: 'Developer',
-    email: 'ipsum@lorem.com',
-    firstName: '',
-    lastName: '',
-    company: 'Google',
-    address: '1600 Amphitheatre Pkwy',
-    city: 'Mountain View',
-    state: 'CA',
-    biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-    postalCode: '94043'
-  };
-  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-    'WY').split(' ').map(function(state) {
-    return {
-      abbrev: state
-    };
-  })
+  $scope.status="hi";
 }])
-
-
-app.config(['$mdThemingProvider', function($mdThemingProvider) {
-  // Configure a dark theme with primary foreground yellow
-  $mdThemingProvider.theme('docs-dark', 'default')
-    .primaryPalette('yellow')
-    .dark();
-}]);
